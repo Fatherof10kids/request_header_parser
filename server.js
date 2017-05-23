ab517c826699ca623f9f20661e78d1e3280274d4
TranManhDung <iamtranmanhdung@gmail.com>
2017-05-23 09:52:16 +0700

    test

--- server.js
+++ server.js
@@ -5,16 +5,54 @@ var port = process.env.PORT || 5000;
 
 app.set('port',port);
 
+// route is for mini app, my code's orgarnization will look better
+// in the future, using route as inside in a custom module
 var route = express.Router();
 
-route.get('/route',(req,res)=>{
-    res.end("this is route");
+// header parser
+route.get('/whoami',(req,res)=>{
+    var ip;
+     var userAgentString = req.headers['user-agent'];
+     var languageString = req.headers['accept-language'];
+
+     // 'x-forwarded-for' header may return multiple IP addresses in
+   // the format: "client IP, proxy 1 IP, proxy 2 IP" so take the
+   // the first one
+     if(req.header('x-forwarded-for')){
+       var arrIp=req.header('x-forwarded-for').split(',');
+       ip = arrIp[0];
+       console.log( req.header('x-forwarded-for').split(',')[0]);
+     }
+     else{
+       ip =req.connection.remoteAddress;
+     }
+
+// get language
+ var arrLang = languageString.split(',');
+ var language = arrLang[0];
+
+// get operating system
+var regex = /\(([^\)]+)\)/;
+var operatingSys = userAgentString.match(regex)[1]; // 
+
+// respond
+var resObj ={
+  "ipaddress" : ip,
+  "language": language,
+  "software": operatingSys
+}
+
+    res.json(resObj);
 });
 
-app.use('/home',route);
+route.get('/another route',(req,res)=>{
+  res.end("this is another route");
+});
+
+app.use('/api',route);
 
 app.get('/home',(req,res)=>{
   res.end("welcome to homepage");
-})
+});
 
 app.listen(port, console.log("server is starting at port",app.get('port')));
